8e66691d4ee5dd6acaefdc13fc6eee67caf6974b
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-04-29 19:21:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,17 @@
 class School  # code here!
-
+#attr_accessor :school 
+#attr_reader : 
 def initialized(school)
 @school = school
 end
+
+SCHOOL = []
+
+
+
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+ 
+  GENRES = []
+ 
