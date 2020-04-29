1e3757067fb7906eed03f6515fbd73651a8aedc5
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-04-29 19:47:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,18 +1,6 @@
-class School  # code here!
-attr_accessor :student, :grade, :roster
-attr_reader :school
-
-def initialize(school)
- @school = school
- end
-
-
-def roster 
-   roster = {} 
+class School
+  
+  
+  def 
  end 
- 
- def add_student(student,grade) 
-   roster[grade] = []
-   roster[grade] << student 
-   end
 end
